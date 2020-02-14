6d651378e6aa0c1db5b359ee26c0e9137439333c
francisco497 <60126666+francisco497@users.noreply.github.com>
2020-02-14 02:56:07 -0600

    Add files via upload

---
+++ PUNTOFIJO2X^2-X-5=0.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+float f(float);
+int main()
+{
+    float x,a,e,maxiter,err;
+    int i=1;
+    printf("\nPUNTO FIJO");
+    printf("\nPrograma para calcular la raiz de 2x^2-x-5=0");
+    printf("\nIndicar el valor inicial de x: ");
+	scanf("%f",&x);
+    printf("\nIndicar el valor del error permitido: ");
+	scanf("%f",&e);
+	printf("\nIndicar el valor de las iteraciones maximas: ");
+	scanf("%f",&maxiter);
+	printf("i \t raiz \t\t error \n");
+    printf("0 \t %f \n",x);
+while(i<=maxiter)
+    {
+
+        a=x;
+        x=f(a);
+        err=fabs(x-a);
+        printf("%d \t %1.4f \t %0.7f\n", i,x,err);
+
+        if(err<=e)
+        {
+             i=maxiter+1;
+        }
+            else
+            {
+                i=i+1;
+            }
+}
+printf("\nEl valor de la raiz es %f",x);
+}
+float f(float x)
+{
+	return sqrt((x+5)/2);
+}
